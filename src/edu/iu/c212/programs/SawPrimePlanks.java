459000fc3dfc85fe8e9de0ab698670c6d9f83366
epautsch <epautsch@iu.edu>
2024-04-22 21:15:14 -0400

    Finished SawPrimePlanks

--- src/edu/iu/c212/programs/SawPrimePlanks.java
+++ src/edu/iu/c212/programs/SawPrimePlanks.java
@@ -1,14 +1,51 @@
 package edu.iu.c212.programs;
 
-import java.util.List;
+import java.util.*;
 
 public class SawPrimePlanks {
 
+    /**
+     * Returns a list of prime plank lengths obtained by sawing a long plank.
+     *
+     * @param longPlankLength The length of the long plank.
+     * @return A list of prime plank lengths.
+     */
     public static List<Integer> getPlankLengths(int longPlankLength){
-        return null;
+        List<Integer> primePlankLengths = new ArrayList<>();
+        int result = sawPlank(longPlankLength);
+        primePlankLengths.add(result);
+        return primePlankLengths;
     }
 
+    /**
+     * Recursively computes the prime factors of a plank length.
+     *
+     * @param plankLength The length of the plank.
+     * @return The product of the prime factors.
+     */
     public static int sawPlank(int plankLength){
-        return 0;
+            if(plankLength == 1){
+                return 1;
+            }
+            int smallestPrimeFactor = findSmallestPrimeFactor(plankLength);
+
+            int result = sawPlank(plankLength / smallestPrimeFactor) * smallestPrimeFactor;
+
+            return result;
+        }
+
+    /**
+     * Finds the smallest prime factor of a number.
+     *
+     * @param n The number to find the smallest prime factor of.
+     * @return The smallest prime factor of the number.
+     */
+    private static int findSmallestPrimeFactor(int n){
+        for(int i = 2; i <= Math.sqrt(n); i++){
+            if(n % i == 0){
+                return i;
+            }
+        }
+        return n;
     }
 }
